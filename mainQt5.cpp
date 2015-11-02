259a32a4f9bf69a56dee802215a23047f1c6292d
P. Most <PMost@PERA-Software.com>
2015-11-02 18:09:07 +0100

    Added more comments

--- mainQt5.cpp
+++ mainQt5.cpp
@@ -17,17 +17,26 @@ int main(int argc, char *argv[]) {
 	QCoreApplication application(argc, argv);
 
 	EchoServerQt5 *echoServer = new EchoServerQt5( &application );
+
+	// Handle the connected signal:
+
 	QObject::connect( echoServer, &EchoServerQt5::connected, []( QTcpSocket *socket ) {
 		out << "Accepted connection from " << *socket << endl;
 	});
+
+	// Handle the disconnected signal:
 
 	QObject::connect( echoServer, &EchoServerQt5::disconnected, []( QTcpSocket *socket ) {
 		out << "Disconnected from " << *socket << endl;
 	});
 
+	// Handle the received data:
+
 	QObject::connect( echoServer, EchoServerQt5::dataReceived, []( QTcpSocket *socket, const QByteArray &data ) {
 		out << "Received data from " << *socket << ": " << data << endl;
 	});
+
+	// Handle that data has been sent:
 
 	QObject::connect( echoServer, EchoServerQt5::dataSent, []( QTcpSocket *socket, qint64 count ) {
 		out << "Sent data to " << *socket << ": " << count << endl;
